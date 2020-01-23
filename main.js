f83886a5edff3008b24781d9d6a8b4b0f72da8bd
xiraynedev <59398743+xiraynedev@users.noreply.github.com>
2020-01-22 23:58:35 -0500

    modified js file using promise chaining

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 const btnRequest = document.getElementById('btnRequest');
 const jsonArea = document.getElementById('jsonArea');
 
-const getData = (callback) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(null, data);
-    } else if (request.readyState === 4) {
-      callback('Could not fetch the data', null);
-    }
-  })
-
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-  request.send(); 
+const getData = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('Error getting resource');
+      }
+    })
+  
+    request.open('GET', resource);
+    request.send(); 
+  });
 }
 
 // attach button event listener to fetch data
 btnRequest.addEventListener('click', function() {
   jsonArea.value = '';
-  getData((err, data) => {
-    if(err) {
-      jsonArea.value = err;
-    } else {
-      const arrayObj = [...data];
-      arrayObj.forEach((item) => {
-        jsonArea.value += item.title;
-      })
-    }
-  });
+  const resource = 'https://jsonplaceholder.typicode.com/todos/';
+  getData(resource).then(data => {
+    const arrayObj = [...data];
+    arrayObj.forEach(item => {
+      jsonArea.value += item.title;
+    })
+  }).catch(err => {
+    jsonArea.value = err;
+  })
 });
