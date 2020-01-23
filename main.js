d6cc6b6fa80efbeb0d1214b959de34bab393d75d
xiraynedev <59398743+xiraynedev@users.noreply.github.com>
2020-01-23 00:15:04 -0500

    replacing js file with fetch api request

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
 const btnRequest = document.getElementById('btnRequest');
 const jsonArea = document.getElementById('jsonArea');
 
-const getData = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject('Error getting resource');
-      }
-    })
-  
-    request.open('GET', resource);
-    request.send(); 
-  });
-}
-
 // attach button event listener to fetch data
 btnRequest.addEventListener('click', function() {
   jsonArea.value = '';
   const resource = 'https://jsonplaceholder.typicode.com/todos/';
-  getData(resource).then(data => {
+  
+  fetch(resource).then(response => response.json())
+  .then(data => {
     const arrayObj = [...data];
     arrayObj.forEach(item => {
       jsonArea.value += item.title;
